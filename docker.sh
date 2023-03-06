811a75b9492aca4ffcf59fba4fe8852ae63a216b
Ubuntu <ubuntu@ip-172-31-7-2.ap-northeast-1.compute.internal>
2023-03-06 14:19:49 +0000

    changes made in docker.sh

--- docker.sh
+++ docker.sh
@@ -2,6 +2,6 @@
 #scripting for installing docker and docker image build
 apt-get update && apt install docker.io -y
 docker --version
-read -p "Enter the version of docker image:" option
-docker build -t reactapp:$option .
+#read -p "Enter the version of docker image:" option
+docker build -t reactapp:v1 .
 docker images
