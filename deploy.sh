d6bad0070ef140003879abb2717d4364ae7820f2
Ubuntu <ubuntu@ip-172-31-7-2.ap-northeast-1.compute.internal>
2023-03-07 16:24:31 +0000

    1

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 ls -l
 mkdir react_app && mv -f * react_app
 tar -cvzf react_app.tar.gz react_app
-scp -o StrictHostKeyChecking=no -i $key react_app.tar.gz ubuntu@18.183.89.72 :/home/ubuntu
+scp -o StrictHostKeyChecking=no -i $key react_app.tar.gz ubuntu@18.183.89.72:/home/ubuntu
 #ssh -T -o StrictHostKeyChecking=no -i $key ubuntu@18.183.89.72<<EOF
 #cd /home/ubuntu
 #rm -rf react_app || true
