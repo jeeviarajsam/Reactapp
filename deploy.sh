594956a43141ceae71978d041842f6d59edcefbc
Ubuntu <ubuntu@ip-172-31-7-2.ap-northeast-1.compute.internal>
2023-03-08 14:10:27 +0000

    4

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@
 #deploy the application
 ls -l
 mkdir react_app && mv -f * react_app
-tar -cvzf react_app.tar.gz react_app
-scp -o StrictHostKeyChecking=no -i $key react_app.tar.gz ubuntu@3.112.2.162:/home/ubuntu
-ssh -T -o StrictHostKeyChecking=no -i $key ubuntu@3.112.2.162<<EOF
-cd /home/ubuntu
-rm -rf react_app || true
-tar -xvzf react_app.tar.gz 
+#tar -cvzf react_app.tar.gz react_app
+#scp -o StrictHostKeyChecking=no -i $key react_app.tar.gz ubuntu@3.112.2.162:/home/ubuntu
+#ssh -T -o StrictHostKeyChecking=no -i $key ubuntu@3.112.2.162<<EOF
+#cd /home/ubuntu
+#rm -rf react_app || true
+#tar -xvzf react_app.tar.gz 
 cd react_app
 chmod +x docker.sh
 ./docker.sh
 #docker login --username=$docker_username --password=$docker_password
-echo $docker_password | docker login -u $docker_username --password-stdin
-docker image push jeeviarajsam/reactapp_dev:v1
+#echo $docker_password | docker login -u $docker_username --password-stdin
+#docker image push jeeviarajsam/reactapp_dev:v1
 #docker run -d -it --name react_app -p 81:80 jeeviarajsam/reactapp_dev:v1
 
