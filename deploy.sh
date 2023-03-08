db13243b91fb2c3a134e65b762be0a2e23a2fdb5
Ubuntu <ubuntu@ip-172-31-7-2.ap-northeast-1.compute.internal>
2023-03-08 12:25:20 +0000

    changes done

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,5 @@ chmod +x docker.sh
 #docker login --username=$docker_username --password=$docker_password
 echo $docker_password | docker login -u $docker_username --password-stdin
 docker image push jeeviarajsam/reactapp_dev:v1
-docker run -d -it --name react_app -p 81:80 jeeviarajsam/reactapp_dev:v1
+#docker run -d -it --name react_app -p 81:80 jeeviarajsam/reactapp_dev:v1
 
