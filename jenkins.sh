9ba7f0afb84b634c74a77ddf1467b153ed1ecc3f
Ubuntu <ubuntu@ip-172-31-7-2.ap-northeast-1.compute.internal>
2023-03-09 18:46:49 +0000

    jeevi

---
+++ jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $GIT_BRANCH == origin/dev ]]
+then
+echo "this is dev branch"
+#chmod +x deploy.sh
+#sh deploy.sh
+elif [[ $GIT_BRANCH == origin/jeevi ]]
+then
+echo "this is jeevi branch"
+else
+echo "give the correct branch"
+fi
+
